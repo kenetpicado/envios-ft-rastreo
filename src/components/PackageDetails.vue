44dfc568b91e340d8f5ea0ab7e86e522804d6c7d
Kenet <kenetpicado1@gmail.com>
2024-10-20 12:36:18 -0600

    deploy

--- src/components/PackageDetails.vue
+++ src/components/PackageDetails.vue
@@ -2,18 +2,14 @@
   <Transition>
     <div v-if="result.details.length > 0" class="text-center max-w-xl">
       <div v-for="(detail, index) in result.details" :key="detail" class="mb-4 px-2">
-        <span class="px-1" :class="{ 'font-bold text-2xl': index == 0 }">
-          {{ detail }}
-        </span>
+        <span :class="{ 'font-bold text-2xl': index === 0 }" v-html="detail" style="white-space: pre-wrap;"></span>
       </div>
       <template v-if="result.details.length > 1">
         <div v-for="(log, index) in result.logs" class="mb-5" :key="index">
           <div class="flex flex-col items-center justify-center mb-2">
-            <span
-              class="font-bold flex items-center justify-center w-8 h-8 text-white rounded-full"
-              :class="log.date ? 'bg-green-600' : 'bg-gray-300'"
-            >
-              <CheckSvg v-if="log.status == 'ENTREGADO'" />
+            <span class="font-bold flex items-center justify-center w-8 h-8 text-white rounded-full"
+              :class="log.date ? 'bg-green-600' : 'bg-gray-300'">
+              <CheckSvg v-if="log.status == 'ENTREGADO' || log.status === 'Entregado'" />
               <label v-else>
                 {{ index + 1 }}
               </label>
@@ -36,7 +32,7 @@
 <script setup>
 import CheckSvg from '@/components/icons/CheckSvg.vue'
 
-const props = defineProps({
+defineProps({
   result: {
     type: Object,
     required: false
--- src/composables/useTrack.js
+++ src/composables/useTrack.js
@@ -4,13 +4,14 @@ import { ref } from 'vue'
 
 export function useTrack() {
   const BASE_URL = 'https://ft-backend-production.up.railway.app'
+  const SECONDARY_URL = 'https://scrape-it-production.up.railway.app'
   //const BASE_URL = 'http://localhost:3001'
   const searching = ref(false)
   const track = ref('')
 
   const result = ref({
     details: [],
-    logs: [],
+    logs: []
   })
 
   const search = async () => {
@@ -35,9 +36,29 @@ export function useTrack() {
       const { data } = await axios.get(`${BASE_URL}/search`, {
         params: { track: track.value }
       })
+
+      if (data.details.length > 1) {
+        result.value = data
+      } else {
+        searchInEverest()
+      }
+    } catch (error) {
+      toast.error('Error al buscar el paquete')
+    } finally {
+      searching.value = false
+    }
+  }
+
+  async function searchInEverest() {
+    try {
+      searching.value = true
+      const { data } = await axios.get(`${SECONDARY_URL}/everest`, {
+        params: { track: track.value }
+      })
+
       result.value = data
     } catch (error) {
-      toast.error(error?.message || 'Error al buscar el paquete')
+      toast.error('Error al buscar el paquete')
     } finally {
       searching.value = false
     }
